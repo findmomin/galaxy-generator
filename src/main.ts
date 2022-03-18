6cdcccb6f08b9293f697f9728daf4de516e2d47c
Abdul Momin <abdulmomin114477@gmail.com>
2022-03-18 08:00:25 +0600

    Completed building the galaxy

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import * as dat from 'dat.gui';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import './style.css';
 
@@ -19,6 +20,9 @@ const cursor = {
 // Scene
 const scene = new THREE.Scene();
 
+// Debug
+const gui = new dat.GUI({ width: 400 });
+
 // Base camera
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -26,15 +30,137 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   100
 );
-camera.position.x = 4;
-camera.position.y = 2;
-camera.position.z = 5;
+camera.position.x = 3;
+camera.position.y = 3;
+camera.position.z = 3;
 
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 
 // Objects
-// Particles
+// Galaxy
+const parameters = {
+  particleCount: 100000,
+  size: 0.01,
+  radius: 5,
+  branches: 3,
+  spin: 1,
+  randomness: 0.2,
+  randomnessPower: 3,
+  innerColor: '#ff6030',
+  outerColor: '#1b3984',
+};
+
+let geometry: THREE.BufferGeometry | null = null;
+let material: THREE.PointsMaterial | null = null;
+let points: THREE.Points | null = null;
+
+const generateGalaxy = () => {
+  // Remove old galaxy
+  geometry?.dispose();
+  material?.dispose();
+  points && scene.remove(points);
+
+  // Geometry
+  geometry = new THREE.BufferGeometry();
+  const positions = new Float32Array(parameters.particleCount * 3);
+  const colors = new Float32Array(parameters.particleCount * 3);
+
+  const innerColor = new THREE.Color(parameters.innerColor);
+  const outerColor = new THREE.Color(parameters.outerColor);
+
+  for (let i = 0; i < parameters.particleCount; i++) {
+    const radius = Math.random() * parameters.radius;
+    const spinAngle = radius * parameters.spin;
+    const branchAngle =
+      ((i % parameters.branches) / parameters.branches) * Math.PI * 2;
+
+    const randomX =
+      Math.pow(Math.random(), parameters.randomnessPower) *
+      (Math.random() < 0.5 ? 1 : -1);
+    const randomY =
+      Math.pow(Math.random(), parameters.randomnessPower) *
+      (Math.random() < 0.5 ? 1 : -1);
+    const randomZ =
+      Math.pow(Math.random(), parameters.randomnessPower) *
+      (Math.random() < 0.5 ? 1 : -1);
+
+    positions[i * 3] = Math.cos(branchAngle + spinAngle) * radius + randomX;
+    positions[i * 3 + 1] = randomY;
+    positions[i * 3 + 2] = Math.sin(branchAngle + spinAngle) * radius + randomZ;
+
+    const mixedColor = innerColor.clone();
+    mixedColor.lerp(outerColor, radius / parameters.radius);
+
+    colors[i * 3] = mixedColor.r;
+    colors[i * 3 + 1] = mixedColor.g;
+    colors[i * 3 + 2] = mixedColor.b;
+  }
+
+  geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
+  geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3));
+
+  // Material
+  material = new THREE.PointsMaterial({
+    size: parameters.size,
+    sizeAttenuation: true,
+    depthWrite: false,
+    blending: THREE.AdditiveBlending,
+    vertexColors: true,
+  });
+
+  // Points
+  points = new THREE.Points(geometry, material);
+
+  scene.add(points);
+};
+
+generateGalaxy();
+
+gui
+  .add(parameters, 'particleCount')
+  .min(100)
+  .max(1000000)
+  .step(100)
+  .onFinishChange(generateGalaxy);
+gui
+  .add(parameters, 'size')
+  .min(0.001)
+  .max(0.1)
+  .step(0.001)
+  .onFinishChange(generateGalaxy);
+gui
+  .add(parameters, 'radius')
+  .min(0.01)
+  .max(20)
+  .step(0.01)
+  .onFinishChange(generateGalaxy);
+gui
+  .add(parameters, 'branches')
+  .min(2)
+  .max(20)
+  .step(1)
+  .onFinishChange(generateGalaxy);
+gui
+  .add(parameters, 'spin')
+  .min(-5)
+  .max(5)
+  .step(0.001)
+  .onFinishChange(generateGalaxy);
+gui
+  .add(parameters, 'randomness')
+  .min(0)
+  .max(2)
+  .step(0.001)
+  .onFinishChange(generateGalaxy);
+gui
+  .add(parameters, 'randomnessPower')
+  .min(1)
+  .max(10)
+  .step(0.001)
+  .onFinishChange(generateGalaxy);
+gui.addColor(parameters, 'innerColor').onFinishChange(generateGalaxy);
+gui.addColor(parameters, 'outerColor').onFinishChange(generateGalaxy);
 
 scene.add(camera);
 
@@ -59,9 +185,9 @@ window.addEventListener('resize', () => {
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
 
-window.addEventListener('mousemove', event => {
-  cursor.x = event.clientX / sizes.width - 0.5;
-  cursor.y = -(event.clientY / sizes.height - 0.5);
+window.addEventListener('mousemove', e => {
+  cursor.x = e.clientX / sizes.width - 0.5;
+  cursor.y = -(e.clientY / sizes.height - 0.5);
 });
 
 const animate = () => {
